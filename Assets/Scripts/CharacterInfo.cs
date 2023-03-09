b2cf6c2121f4388ef2ed7ed4164e710ad7ef869c
benm404 <99894786+benm404@users.noreply.github.com>
2023-03-09 14:06:13 +0000

    Implemented character animations
    Character runs while running, idles while standing still

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class CharacterInfo : MonoBehaviour
 {
+    private Animator animator;
     public OverlayTile activeTile;
+    private float xPos;
+    private float yPos;
+    private float prevXPos;
+    private float prevYPos;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        xPos = transform.position.x;
+        yPos = transform.position.y;
+
+        if (xPos != prevXPos || yPos != prevYPos)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        prevXPos = transform.position.x;
+        prevYPos = transform.position.y;
+    }
 }
