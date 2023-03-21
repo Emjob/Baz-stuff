0382587bd007c70b98cffffd0682d8573b9d7d12
benm404 <99894786+benm404@users.noreply.github.com>
2023-03-21 17:58:50 +0000

    Changed rigidbody
    Changed Rigidbody from kinematic to dynamic. Changed pickup script to use OnTriggerStay2D rather than OnTriggerEnter2D.

--- Assets/PickUpScript.cs
+++ Assets/PickUpScript.cs
@@ -8,13 +8,14 @@ public class PickUpScript : MonoBehaviour
     public GameObject ElementalObj;
     //public GameObject UI;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.CompareTag("Player"))
         {
+            print("Contact");
             if (Input.GetKeyDown(KeyCode.R))
             {
-                
+                print("Do something");
             }
         }
     }
