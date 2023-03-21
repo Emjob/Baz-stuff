7c852b3e1be3f2e0a00229dd51f85a7638c99b90
benm404 <99894786+benm404@users.noreply.github.com>
2023-03-21 15:16:40 +0000

    Movement finished
    Player can now move on any tilemap so long as it is set up in the script/editor correctly.

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -15,6 +15,9 @@ public class MapManager : MonoBehaviour
     public Dictionary<Vector2Int, OverlayTile> map;
 
     public Tilemap tileMap;
+    public Tilemap tileMap1;
+
+    public bool activateTilemap;
     private void Awake()
     {
         if(instance != null && instance != this)
@@ -33,6 +36,48 @@ public class MapManager : MonoBehaviour
         BoundsInt bounds = tileMap.cellBounds;
 
         for(int z = bounds.max.z; z >= bounds.min.z; z--)
+        {
+            for (int y = bounds.min.y; y < bounds.max.y; y++)
+            {
+                for (int x = bounds.min.x; x < bounds.max.x; x++)
+                {
+                    var tileLocation = new Vector3Int(x, y, z);
+                    var tileKey = new Vector2Int(x, y);
+
+                    if (tileMap.HasTile(tileLocation) && !map.ContainsKey(tileKey))
+                    {
+                        var overlayTile = Instantiate(overlayTilePrefab, overlayContainer.transform);
+                        var cellWorldPosition = tileMap.GetCellCenterWorld(tileLocation);
+
+                        overlayTile.transform.position = new Vector3(cellWorldPosition.x, cellWorldPosition.y - 0.09f, cellWorldPosition.z + 1);
+                        overlayTile.GetComponent<SpriteRenderer>().sortingOrder = tileMap.GetComponent<TilemapRenderer>().sortingOrder + 1;
+                        overlayTile.gridLocation = tileLocation;
+                        map.Add(tileKey, overlayTile);
+                    }
+                }
+            }
+        }
+
+        
+    }
+
+    private void Update()
+    {
+        if (activateTilemap)
+        {
+            disableTilemap(tileMap1);
+        }
+    }
+
+    private void disableTilemap(Tilemap tileMap)
+    {
+        activateTilemap = false;
+
+        tileMap.gameObject.SetActive(true);
+
+        BoundsInt bounds = tileMap.cellBounds;
+
+        for (int z = bounds.max.z; z >= bounds.min.z; z--)
         {
             for (int y = bounds.min.y; y < bounds.max.y; y++)
             {
