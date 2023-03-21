55f5b1a60da7b114cae23048f7cc9d7c2e06c674
Justin Rhay Samson <99894685+JustinRhaySamson@users.noreply.github.com>
2023-03-21 10:35:18 +0000

    Level Update _ Colliedrs

--- Assets/nextScene.cs
+++ Assets/nextScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class nextScene : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class nextScene : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
     }
 }
