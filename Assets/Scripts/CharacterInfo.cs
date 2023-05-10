8a12ab7570f66f60fbc0641644871d16675fe019
benm404 <99894786+benm404@users.noreply.github.com>
2023-05-10 12:17:56 +0100

    Update CharacterInfo.cs

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -16,7 +15,6 @@ public class CharacterInfo : MonoBehaviour
     private GameObject DeathScreen;
 
     private MouseController mouseController;
-
     private PickUpScript PickUpScript;
 
     public bool isPlayerDead;
